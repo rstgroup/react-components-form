8726e5c84381e7cbcb65db30d90dc4ee11b45a2d
Mariusz Przodała <mariusz.przodala@proserwit.pl>
2017-03-05 20:15:35 +0100

    ghpages

---
+++ webpack.ghpages.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: ['babel-polyfill', "./src/demo/index.jsx"],
+    output: {
+        path: 'dist/',
+        filename: "main.js"
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+            },
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'stage-0', 'es2017', 'react']
+                }
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    }
+};
