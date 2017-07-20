0312504774c7d0e26ae48b7077abc12abe1810d9
karol <karol.sobolewski@netins.it>
2017-07-20 12:13:46 +0200

    ListField reset event tests

---
+++ tests/data/models.js
@@ -0,0 +1,25 @@
+export const listModel = {
+    title: 'titleTest',
+    authors: [
+        {
+            name: 'te',
+            surname: 'tes',
+        },
+        {
+            name: 'Test3',
+            surname: 'Test3',
+        },
+    ],
+    languages: ['test', 'test2', 'test3'],
+}
+
+export const newListModel = {
+    title: '',
+    authors: [
+        {
+            name: '',
+            surname: '',
+        },
+    ],
+    languages: [''],
+};
--- tests/data/schemas.js
+++ tests/data/schemas.js
@@ -35,3 +35,28 @@ export const listSchema = new Schema({
         type: [listElementSchema]
     }
 });
+
+const personSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String
+    },
+});
+
+export const resetSchema = new Schema({
+    title:{
+        type: String,
+        label: 'Title',
+        required: true
+    },
+    authors:{
+        type: [personSchema],
+        label: 'Authors'
+    },
+    languages:{
+        type: [String]
+    },
+});
