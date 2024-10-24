5c97fbf6ff42084dac8204ba96f017aabb0eb72c
Mariusz Przodała <mariuszprzodala@Mariuszs-MacBook-Pro-8.local>
2024-10-24 07:31:57 +0200

    fix: tests

--- src/tests/FormController.test.js
+++ src/tests/FormController.test.js
@@ -1,70 +1,102 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React, { useRef, useEffect } from 'react';
+import { render, waitFor, act } from '@testing-library/react';
 import Form from '../Form';
 import FormController from '../FormController';
 
 describe('FormController', () => {
     let form;
     beforeEach(() => {
-        const { container } = render(<Form onSubmit={() => {}} />);
-        form = container.firstChild._reactInternals.child.stateNode;
+        const FormWrapper = () => {
+            const formRef = useRef();
+            useEffect(() => {
+                form = formRef.current;
+            }, []);
+            return <Form ref={formRef} onSubmit={() => {}} />;
+        };
+        render(<FormWrapper />);
     });
 
     it('should set form instance in controller', () => {
         const controller = new FormController();
         expect(controller.form).toEqual({});
-        controller.setForm(form);
+        act(() => {
+            controller.setForm(form);
+        });
         expect(controller.form).toEqual(form);
     });
 
     it('should get form instance from controller', () => {
         const controller = new FormController();
-        controller.setForm(form);
+        act(() => {
+            controller.setForm(form);
+        });
         expect(controller.getForm()).toEqual(form);
     });
 
-    it('should set new schema in form instance', () => {
+    it('should set new schema in form instance', async () => {
         const controller = new FormController();
         const schema = { validate: () => ({}) };
-        controller.setForm(form);
+        act(() => {
+            controller.setForm(form);
+        });
         expect(form.state.schema).toEqual({});
-        controller.setSchema(schema);
-        expect(form.state.schema).toEqual(schema);
+        act(() => {
+            controller.setSchema(schema);
+        });
+        await waitFor(() => {
+            expect(form.state.schema).toEqual(schema);
+        });
     });
 
-    it('should get schema from form instance', () => {
+    it('should get schema from form instance', async () => {
         const controller = new FormController();
         const schema = { validate: () => ({}) };
-        controller.setForm(form);
-        controller.setSchema(schema);
-        expect(controller.getSchema()).toEqual(schema);
+        act(() => {
+            controller.setForm(form);
+            controller.setSchema(schema);
+        });
+        await waitFor(() => {
+            expect(controller.getSchema()).toEqual(schema);
+        });
     });
 
-    it('should get validation errors from form instance', () => {
+    it('should get validation errors from form instance', async () => {
         const controller = new FormController();
         const mockedErrors = { foo: 'bar' };
         form.state.validationErrors = mockedErrors;
-        controller.setForm(form);
-        expect(controller.getErrors()).toEqual(mockedErrors);
+        act(() => {
+            controller.setForm(form);
+        });
+        await waitFor(() => {
+            expect(controller.getErrors()).toEqual(mockedErrors);
+        });
     });
 
-    it('should set field value', () => {
+    it('should set field value', async () => {
         const controller = new FormController();
         const name = 'foo';
         const value = 'bar';
         form.setModel = jest.fn();
-        controller.setForm(form);
-        controller.setFieldValue(name, value);
-        expect(form.setModel).toHaveBeenCalledWith(name, value);
+        act(() => {
+            controller.setForm(form);
+            controller.setFieldValue(name, value);
+        });
+        await waitFor(() => {
+            expect(form.setModel).toHaveBeenCalledWith(name, value);
+        });
     });
 
-    it('should get field value', () => {
+    it('should get field value', async () => {
         const controller = new FormController();
         const name = 'foo';
         const value = 'bar';
         form.getModel = jest.fn(() => value);
-        controller.setForm(form);
-        expect(controller.getFieldValue(name)).toEqual(value);
-        expect(form.getModel).toHaveBeenCalledWith(name);
+        act(() => {
+            controller.setForm(form);
+        });
+        await waitFor(() => {
+            expect(controller.getFieldValue(name)).toEqual(value);
+            expect(form.getModel).toHaveBeenCalledWith(name);
+        });
     });
 });
